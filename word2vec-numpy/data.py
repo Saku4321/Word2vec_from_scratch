a0f0197f9404fef96aad8d4aa9feb2a99623e0f5
Jakub <01197854@pw.edu.pl>
2026-03-22 10:43:10 +0100

    STOP_WORDS_added

--- word2vec-numpy/data.py
+++ word2vec-numpy/data.py
@@ -1,4 +1,7 @@
 import numpy as np
+STOP_WORDS = {"a", "an", "the", "is", "are", "was", "were", "in",
+              "on", "at", "to", "of", "and", "with", "for",
+              "from", "by", "it", "its", "that", "also", "but", "or"}
 def load_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
         text =f.read().lower()
@@ -8,7 +11,7 @@ def load_text(filepath):
 def build_vocab(words, min_count=2):
     from collections import Counter
     count = Counter(words)
-    vocab = [w for w, c in count.items() if c >= min_count]
+    vocab = [w for w, c in count.items() if c >= min_count and w not in STOP_WORDS]
     word2idx = {w: i for i, w in enumerate(vocab)}
     idx2word = {i: w for i, w in enumerate(vocab)}
     return word2idx, idx2word
